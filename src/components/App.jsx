4ca0c91a61b0c0a88ccde9340ec4031fbeef9ffb
VitaliyDunyushkin <117518206+VitaliyDunyushkin@users.noreply.github.com>
2023-07-06 09:56:24 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route if path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
